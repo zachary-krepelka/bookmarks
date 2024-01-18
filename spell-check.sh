6998f99779e68101ce8b1dde42afb0199e36009c
Zachary Krepelka <155400781+zachary-krepelka@users.noreply.github.com>
2024-01-18 17:43:12 -0500

    Added remark

--- spell-check.sh
+++ spell-check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# FILENAME: spell.sh
+# FILENAME: spell-check.sh
 # AUTHOR: Zachary Krepelka
 # DATE: Thursday, January 18th, 2024
 
@@ -9,6 +9,12 @@
 	# The purpose of this script is
 	# to identify spelling mistakes
 	# within bookmark entries.
+
+# REMARK
+
+	# It may help to pipe the result into less with the -R flag, like this:
+
+		# bash spell-check.sh bookmarks.html | less -R
 
 if [[ $# -ne 1 ]]; then
 	echo 'Exactly one argument is required.' 1>&2
