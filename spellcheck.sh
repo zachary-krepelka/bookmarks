0fefb9e876ab3faba8cace7f214b54539893b2e0
Zachary Krepelka <155400781+zachary-krepelka@users.noreply.github.com>
2024-05-22 02:17:08 -0400

    added ignore-list feature

--- spellcheck.sh
+++ spellcheck.sh
@@ -3,28 +3,52 @@
 # FILENAME: spellcheck.sh
 # AUTHOR: Zachary Krepelka
 # DATE: Thursday, January 18th, 2024
-# UPDATED: Thursday, March 28th, 2024 at 8:40 PM
-
-# PURPOSE
+# ABOUT: a bookmark spellchecking program
+# ORIGIN: https://github.com/zachary-krepelka/bookmarks.git
+# UPDATED: Wednesday, May 22nd, 2024 at 2:06 AM
 
 	# The purpose of this script is
 	# to identify spelling mistakes
 	# within bookmark entries.
 
-usage() {
-	PROG=$(basename $0)
-	cat <<-EOF >&2
-		Usage: $PROG [option] <file>
-		spellcheck your bookmark entries
+usage() { program=$(basename $0); cat << EOF >&2
+Usage: $program [option] <bookmark file>
+spellcheck your bookmark entries on the command line
 
-		Documentation:  perldoc $PROG
-		Options:        -h to display this help message
-		Example:        bash $PROG bookmarks.html
-	EOF
-	exit 0
+Options:
+
+	-h		display this [h]elp message
+	-i {file}	{file} is a list of words to [i]gnore
+			It must begin with "personal_ws-1.1 en".
+
+Documentation:  perldoc $PROG
+Example:        bash $PROG bookmarks.html
+EOF
+exit 0
 }
 
-[ "$1" == "-h" -o "$1" == "--help" ] && usage
+while getopts hi: option
+do
+	case $option in
+
+		h) usage;;
+
+		i)
+
+			ignore_list=$OPTARG
+
+			# Update the word count so the user doesn't have to
+
+			word_count=$(expr $(wc -l < $ignore_list) - 1)
+
+			sed -i "s/\(personal_ws-1.1 en \)\w*/\1$word_count/" $ignore_list
+
+		;;
+
+	esac
+done
+
+shift $((OPTIND-1))
 
 if [[ $# -ne 1 ]]
 then
@@ -36,7 +60,13 @@ set -f # disable globbing
 
 while read line; do
 
-	echo $line | grep -F --color=ALWAYS -f <(echo $line | aspell list)
+	echo $line | grep -F --color=ALWAYS -f <( echo $line |
+                                                             \
+		aspell list ${ignore_list:+                  \
+                                                             \
+			--home-dir=$(dirname $ignore_list)   \
+			--personal=$ignore_list              \
+	})
 
 done < <(sed -f - <(grep -Po '(?<=>)[^<]*(?=</A>)' $1) <<-EOF |
 
@@ -65,6 +95,21 @@ bash spellcheck.sh <bookmark-file>
 The purpose of this script is to identify spelling mistakes within bookmark
 entries.  The input is a file in the Netscape Bookmark file format.  The output
 is a list of bookmark entries with spelling mistakes highlighted.
+
+=head1 OPTIONS
+
+=over
+
+=item B<-h>
+
+Display a [h]elp message and exit.
+
+=item B<-i> I<FILE>
+
+This flag specifies a file containing a list of words to [i]gnore.  The file
+must begin with the string "personal_ws-1.1 en".
+
+=back
 
 =head1 NOTES
 
